fc9b4053f039cbacfa39486c84537d1bf7dd346d
Carlos Blanco <carlitocs@estudiante.uc.cl>
2025-10-23 21:00:44 -0300

    Final Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "learning-astro-carlito.netlify.app"
+  site: "https://learning-astro-carlitoito.netlify.app",
+  integrations: [preact()]
 });
